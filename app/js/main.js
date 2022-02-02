a9423ff73024b03fd9f60399957f9ddf8a466907
Roman Denysenko <roman20052007@gmail.com>
2022-02-03 01:41:54 +0200

    design first sheet aside bar

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(function(){
 
     $('.header__slider').slick({
-        infinite: true
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="right"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="right"></img>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.slider-dots'
+    });
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+        dots: true,
     });
 
   
