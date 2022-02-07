2573fbfd0984ae4b97a16abcba11c596efa5951c
Roman Denysenko <roman20052007@gmail.com>
2022-02-08 00:51:42 +0200

    second sheet slider-need to fix

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,12 @@ $(function(){
         asNavFor: '.header__slider',
         dots: true,
     });
-
+    $('.serf__slider').slick({
+        
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="right"></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="right"></img>',
+        slidesToShow: 4,
+        slidesToScroll: 1,      
+    })
   
 });
