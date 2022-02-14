0a15a59ba13516c7db810bf027ede212cd00f9c6
Roman Denysenko <roman20052007@gmail.com>
2022-02-14 17:55:06 +0200

    second sheet

--- app/js/main.js
+++ app/js/main.js
@@ -7,20 +7,27 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="right"></img>',
         slidesToShow: 1,
         slidesToScroll: 1,
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
         dots: true,
     });
-    $('.serf__slider').slick({
+    $('.surf-slider').slick({
         
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrow-left.svg" alt="right"></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrow-right.svg" alt="right"></img>',
         slidesToShow: 4,
         slidesToScroll: 1,      
+        asNavFor: '.slider-map',
     })
-  
+   $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,  
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect:true,
+    })
 });
